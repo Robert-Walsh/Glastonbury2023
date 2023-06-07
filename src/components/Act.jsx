392511354c70df348fc89358c1d96b467e1c253d
Robert Walsh <robert.walsh1411@gmail.com>
2023-06-07 23:13:22 +0100

    noreferrer

--- src/components/Act.jsx
+++ src/components/Act.jsx
@@ -7,7 +7,7 @@ function Act({stage, name, time, day, selected, onClick}){
     <div onClick={handleClick} style={{'cursor': 'pointer', 'backgroundColor': selected ? '#b8e994' : '', 'margin-left': '10px'}}>
       <div style={{'fontWeight':'bold'}}>
         {name}
-        <a href={`https://open.spotify.com/search/artist:${name}`} target="_blank" style={{'position': 'absolute', 'right': '0', 'margin': '15px'}}>
+        <a href={`https://open.spotify.com/search/artist:${name}`} target="_blank" rel="noreferrer" style={{'position': 'absolute', 'right': '0', 'margin': '15px'}}>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" style={{'width': '40px'}}/>
         </a>
       </div>
--- src/components/PlanAct.jsx
+++ src/components/PlanAct.jsx
@@ -3,7 +3,7 @@ function PlanAct({stage, name, time}){
     <div style={{'margin-left': '10px'}}>
       <div style={{'fontWeight':'bold'}}>
         {name}
-        <a href={`https://open.spotify.com/search/artist:${name}`} target="_blank" style={{'position': 'absolute', 'right': '0', 'margin': '15px'}}>
+        <a href={`https://open.spotify.com/search/artist:${name}`} target="_blank" rel="noreferrer" style={{'position': 'absolute', 'right': '0', 'margin': '15px'}}>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" style={{'width': '40px'}}/>
         </a>
       </div>
