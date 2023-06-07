174fb11547695a3d85bc130bc2dc69ef303fc2b3
Robert Walsh <robert.walsh1411@gmail.com>
2023-06-07 23:16:00 +0100

    alt props

--- src/components/PlanAct.jsx
+++ src/components/PlanAct.jsx
@@ -4,7 +4,7 @@ function PlanAct({stage, name, time}){
       <div style={{'fontWeight':'bold'}}>
         {name}
         <a href={`https://open.spotify.com/search/artist:${name}`} target="_blank" rel="noreferrer" style={{'position': 'absolute', 'right': '0', 'margin': '15px'}}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" style={{'width': '40px'}}/>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" style={{'width': '40px'}} alt="spotify"/>
         </a>
       </div>
       <div>
