749fec37595e314c3edf3bbf6f06aa2132dc5e20
Robert Walsh <robert.walsh1411@gmail.com>
2023-06-08 19:07:52 +0100

    clashes

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,25 +1,30 @@
 import PlanAct from "./PlanAct"
+import { AddClashes } from '../services/addClashes'
 
 function Plan({selectedActs}) {
   const fridayActs = selectedActs.filter(x => x.day === 'FRIDAY').sort((a, b) => a.time > b.time ? 1 : -1)
   const saturdayActs = selectedActs.filter(x => x.day === 'SATURDAY').sort((a, b) => a.time > b.time ? 1 : -1)
   const sundayActs = selectedActs.filter(x => x.day === 'SUNDAY').sort((a, b) => a.time > b.time ? 1 : -1)
 
+  AddClashes(fridayActs)
+  AddClashes(saturdayActs)
+  AddClashes(sundayActs)
+
   const mappedFridayActs = fridayActs.map((act) => {
     return (
-      <PlanAct key={act.name} name={act.name} stage={act.stage} time={act.time} mustSee={act.mustSee} mightSee={act.mightSee}/>
+      <PlanAct key={act.name} name={act.name} stage={act.stage} time={act.time} mustSee={act.mustSee} mightSee={act.mightSee} clash={act.clash}/>
     )
   })
 
   const mappedSaturdayActs = saturdayActs.map((act) => {
     return (
-      <PlanAct key={act.name} name={act.name} stage={act.stage} time={act.time} mustSee={act.mustSee} mightSee={act.mightSee}/>
+      <PlanAct key={act.name} name={act.name} stage={act.stage} time={act.time} mustSee={act.mustSee} mightSee={act.mightSee} clash={act.clash}/>
     )
   })
 
   const mappedSundayActs = sundayActs.map((act) => {
     return (
-      <PlanAct key={act.name} name={act.name} stage={act.stage} time={act.time} mustSee={act.mustSee} mightSee={act.mightSee}/>
+      <PlanAct key={act.name} name={act.name} stage={act.stage} time={act.time} mustSee={act.mustSee} mightSee={act.mightSee} clash={act.clash}/>
     )
   })
 
@@ -27,18 +32,20 @@ function Plan({selectedActs}) {
   return (
     <div>
       <div className="card" style={{'margin': '20px'}}>
-        <div className="title" style={{'marginLeft': '10px', 'paddingTop': '10px'}}>Friday</div>
+        <div className="title" style={{'paddingTop': '25px', 'textAlign': 'center'}}>Friday</div>
+        <hr/>
+
         {mappedFridayActs}
       </div>
       
       <div className="card" style={{'margin': '20px'}}>
-        <div className="title" style={{'marginLeft': '10px', 'paddingTop': '10px'}}>Saturday</div>
+        <div className="title" style={{'paddingTop': '25px', 'textAlign': 'center'}}>Saturday</div>
         {mappedSaturdayActs}
       </div>
 
 
       <div className="card" style={{'margin': '20px'}}>
-        <div className="title" style={{'marginLeft': '10px', 'paddingTop': '10px'}}>Sunday</div>
+        <div className="title" style={{'paddingTop': '25px', 'textAlign': 'center'}}>Sunday</div>
         {mappedSundayActs}
       </div>
     </div>
--- src/components/PlanAct.jsx
+++ src/components/PlanAct.jsx
@@ -1,5 +1,4 @@
-function PlanAct({stage, name, time, mustSee, mightSee}){
-  
+function PlanAct({stage, name, time, mustSee, mightSee, clash}){
   const mustSeeBox = (
     <div style={{'backgroundColor': '#b8e994', 'borderRadius': '4px', 'textAlign': 'center', 'width': '18%', 'border': 'solid 1px grey', 'fontSize': '10px', 'fontWeight': 'bold', 'marginTop': '4px', 'boxShadow': '0 8px 6px -6px black'}}>
       <div style={{'padding': '4px'}}>
@@ -15,24 +14,36 @@ function PlanAct({stage, name, time, mustSee, mightSee}){
       </div>
     </div>
   )
-  
+
+  const clashBox = (
+    <div style={{'backgroundColor': '#e74c3c', 'color': '#ecf0f1', 'borderRadius': '4px', 'textAlign': 'center', 'width': '18%', 'border': 'solid 1px grey', 'fontSize': '10px', 'fontWeight': 'bold', 'marginTop': '4px', 'boxShadow': '0 8px 6px -6px black'}}>
+      <div style={{'padding': '4px'}}>
+        Clash
+      </div>
+    </div>
+  )
+
   return (
-    <div style={{'marginLeft': '10px'}}>
-      <div style={{'fontWeight':'bold'}}>
-        {name}
-        <a href={`https://open.spotify.com/search/artist:${name}`} target="_blank" rel="noreferrer" style={{'position': 'absolute', 'right': '0', 'margin': '15px'}}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" style={{'width': '40px'}} alt="spotify"/>
-        </a>
+    <div>
+      <div style={{'padding': '4px', 'marginLeft': '10px', 'marginTop': '0px'}}>
+        <div style={{'fontWeight':'bold'}}>
+          {name}
+          <a href={`https://open.spotify.com/search/artist:${name}`} target="_blank" rel="noreferrer" style={{'position': 'absolute', 'right': '0', 'margin': '15px'}}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" style={{'width': '40px'}} alt="spotify"/>
+          </a>
+        </div>
+        <div>
+          {stage}
+        </div>
+        <div>
+          {time}
+        </div>
+        {mustSee && mustSeeBox}
+        {mightSee && mightSeeBox}
+        {clash && clashBox}
+       <hr/>
       </div>
-      <div>
-        {stage}
-      </div>
-      <div>
-        {time}
-      </div>
-      {mustSee && mustSeeBox}
-      {mightSee && mightSeeBox}
-      <hr/>
+      
     </div>
   )
 }
